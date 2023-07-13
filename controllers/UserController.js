6baf3fc137eedc7ad6eda5f82ff1334f7ddb2969
vietnd <nguyenviet4664@gmail.com>
2023-07-13 23:42:57 +0700

    api getUserByEmail

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,6 +15,7 @@
 //     }
 // ]
 
+const { query } = require('express')
 const User = require('../models/UserModel')
 
 class UserController {
@@ -36,22 +37,51 @@ class UserController {
     // }
 
     getUserByEmail(req, res) {
-        // User.find({ useremail: req.params.useremail.toString() })
-        // const query = User.where({ this.getUserByEmail})
-        const inputStr = req.params.useremail.toString()
-        // User.findOne({ "useremail": req.params.useremail })
-        // ? User.findOne({ "useremail": 'tna@gmail.com' })
-        // User.find({ "useremail": { $req.params.useremail } })
-        User.find({ useremail: 'tna@gmail.com' })
+        const userEmailValue = req.params.useremail
+
+        User.find({ useremail: userEmailValue }).exec()
             .then(user => {
-                if (user) {
+                if (user != null) {
+                    res.append('Access-Control-Allow-Origin', ['*'])
+                    res.append('Access-Control-Allow-Methods', 'DELETE,GET,PATCH,POST,PUT')
+                    res.append('Access-Control-Allow-Headers', 'Content-Type,Authorization')
                     res.json(user)
                 } else {
                     res.json({ error: 'User ${inputStr} not found ' + inputStr })
                 }
             })
-            .catch(err => res.status(404).json({ error: 'Backend error' }))
+            .catch(err => res.status(500).json({ error: 'Backend Error' }))
     }
+
+    createUser(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+
+        const user = {
+            username: req.body.username
+
+        }
+        res.json(req.body)
+
+    }
+
+
+    // app.post('/api/rooms/add', (req, res) => {
+    //     console.log(req.body)
+    //     res.setHeader('X-Powered-By', 'Node.js')
+
+    //     const room = {
+    //         id: req.body.id,
+    //         name: req.body.name
+    //     }
+    //     rooms.push(room)
+
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.send(JSON.stringify({
+    //         success: "true",
+    //         notice: "Add thanh cong",
+    //         data: rooms
+    //     }))
+    // })
 
 
     list(req, res) {
